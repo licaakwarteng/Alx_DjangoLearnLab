ac7006139081008f8f1abc2f7242a0419c05b307
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-09-02 14:06:40 +0000

    created query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -22,8 +22,9 @@ def run_queries():
     print(f"Books in {library_name}: {[book.title for book in books_in_library]}")
 
     # 3. Retrieve the librarian for a library
-    librarian_name = Librarian.objects.get(name="Ama Forson")
-    print(f"Librarian of {library_name}: {librarian_name}")
+    librarian_name = Librarian.objects.get(library="Central Library")
+    librarian = Librarian.objects.get(name="Ama Forson")
+    print(f"Librarian of {library_name}: {librarian.name}")
 
 
 if __name__ == "__main__":
