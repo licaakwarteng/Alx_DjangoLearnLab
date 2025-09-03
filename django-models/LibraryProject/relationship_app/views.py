438c39191c9e60ca32f10e7610c06e118a53b778
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-09-03 19:54:58 +0000

    relationship app updated with admin view, librarian and member view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -60,3 +60,35 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, "relationship_app/logout.html")
+
+
+from django.contrib.auth.decorators import user_passes_test, login_required
+
+# Role check functions
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+# Views restricted by role
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+@login_required
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
