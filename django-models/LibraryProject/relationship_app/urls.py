f3e355fb384d8a2d0ca47d131876b5fd98ee7e08
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-09-03 20:24:20 +0000

    updated urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("logout/",LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
     # Role-based views
-    path("admin-dashboard/", views.admin_view, name="admin_view"),
-    path("librarian-dashboard/", views.librarian_view, name="librarian_view"),
-    path("member-dashboard/", views.member_view, name="member_view"),
+    path("Admin/", views.admin_view, name="admin_view"),
+    path("Librarian/", views.librarian_view, name="librarian_view"),
+    path("Member/", views.member_view, name="member_view"),
     path("redirect/", views.role_redirect_view, name="role_redirect"),
 ]
