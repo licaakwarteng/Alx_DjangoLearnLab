733d4360d99cdccf1e0421f501f19db1ab2ae25d
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-09-10 12:19:52 +0000

    updating my bookshelf app with advanced features

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.book_list, name="book_list"),
+    path("books/create/", views.book_create, name="book_create"),
+    path("books/<int:book_id>/edit/", views.book_edit, name="book_edit"),
+    path("books/<int:book_id>/delete/", views.book_delete, name="book_delete"),
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "books/book_list.html", {"books": books})
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def book_create(request):
+    return HttpResponse("📖 Book creation page (only for users with can_create).")
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return HttpResponse(f"✏️ Edit page for {book.title} (only for users with can_edit).")
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return HttpResponse(f"🗑️ Delete page for {book.title} (only for users with can_delete).")
