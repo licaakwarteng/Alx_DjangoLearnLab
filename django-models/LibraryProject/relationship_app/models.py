890bdd734468f514909bd81c6ec0784aaf75952d
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-09-26 16:48:51 +0000

    setup done

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,31 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ("Admin", "Admin"),
-        ("Librarian", "Librarian"),
-        ("Member", "Member"),
-    ]
+# class UserProfile(models.Model):
+#     ROLE_CHOICES = [
+#         ("Admin", "Admin"),
+#         ("Librarian", "Librarian"),
+#         ("Member", "Member"),
+#     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
 
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
+#     def __str__(self):
+#         return f"{self.user.username} - {self.role}"
 
 
-# --- Signals: auto-create/save profile for each new User ---
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+# # --- Signals: auto-create/save profile for each new User ---
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.userprofile.save()
 
     
 
