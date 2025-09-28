2dfc71299be6b88cc62bd23b9410454b239fc5be
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-09-28 13:02:57 +0000

    created tests for the api

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
 
 ROOT_URLCONF = 'advancedapiproject.urls'
 
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,17 +1,28 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, filters
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 class BookListView(generics.ListAPIView):
     """
     GET /books/ - List all books.
     Open to all users (read-only).
+
+    - Supports filtering by 'title', 'author', and 'publication_year'.
+    e.g., /api/books/?author=1
+    - Supports search by 'title' and author's name.
+    e.g., /api/books/?search=tolkien
+    - Supports ordering by 'title' or 'publication_year'.
+    e.g., /api/books/?ordering=-publication_year
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]  # Read-only, open access
-
+    #filtering books
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year'] #ordering search by fields
 
 class BookDetailView(generics.RetrieveAPIView):
     """
@@ -51,3 +62,12 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    
+
+    
