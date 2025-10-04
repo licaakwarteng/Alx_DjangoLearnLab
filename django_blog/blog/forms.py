8d41e434c3daeaee5ef7aa4e7936f19ef7bd7533
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-10-04 18:33:08 +0000

    Added more templates

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Post title'}),
+            'content': forms.Textarea(attrs={'rows': 8, 'placeholder': 'Write your post...'}),
+        }
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,15 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+from .forms import PostForm
+from .forms import RegisterForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
 
 def register(request):
     if request.method == 'POST':
@@ -26,3 +25,45 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html')
+
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'   # blog/templates/blog/post_list.html
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+    paginate_by = 10  # optional
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'   # reused for create & update
+
+    def form_valid(self, form):
+        # set the author to the logged-in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
