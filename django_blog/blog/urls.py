6ed7efd731cecc9a05d819038dafaadff5185346
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-10-04 18:58:58 +0000

    Updated urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
