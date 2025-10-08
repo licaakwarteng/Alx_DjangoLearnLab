8c2a7ac7fb24ec65f135b9ca480b94dfe19a477a
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-10-08 18:45:06 +0000

    Updated an accounts and posts apps

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions, generics
 from .models import Post, Comment
+from rest_framework.decorators import action
 from .serializers import PostSerializer, CommentSerializer
 
 
@@ -11,7 +12,7 @@ class FeedView(generics.ListAPIView):
         # Get posts from followed users
         user = self.request.user
         followed_users = user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
     
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -33,6 +34,17 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def feed(self, request):
+        """
+        Returns posts from users that the current user follows.
+        """
+        user = request.user
+        following_users = user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
